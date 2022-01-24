cf616f68797a074d535e3b92c3492baed9dfc4b2
SamRathore01 <96233202+SamRathore01@users.noreply.github.com>
2022-01-24 15:18:48 +0800

    changes

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -1,10 +1,16 @@
+import ProjectImg from '../assets/Images/projectImg.png';
+import UTrackerImg from '../assets/Images/utracker.jpg';
+import GreenCtgImg from '../assets/Images/f.png';
+import CoinTrackerImg from '../assets/Images/cointracker.jpg';
+import CavinImg from '../assets/Images/cavinimg.jpg';
+
 export const Blogs = [
     {
         id:1,
         name:"Weather Fetch Website",
         tags:["react","Axios","styled-components"],
         date:"13 Jan, 2022",
-        imgSrc:"",
+        imgSrc:UTrackerImg,
         link:"https://wdt.netlify.app/"
     },
     {
@@ -12,7 +18,7 @@ export const Blogs = [
         name:"Tax Reutn",
         tags:["react","styled-components"],
         date:"5 July, 2021",
-        imgSrc:"",
+        imgSrc:GreenCtgImg,
         link:"https://taxsitez.netlify.app"
     },
     {
@@ -20,7 +26,7 @@ export const Blogs = [
         name:"React Loading Screen: Try these 3 cool loading screens for your app",
         tags:["react","react-lottie","styled-components"],
         date:"6 July, 2021",
-        imgSrc:"",
+        imgSrc: CoinTrackerImg,
         link:""
     },
     {
@@ -28,7 +34,7 @@ export const Blogs = [
         name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
         tags:["react","redux","framer-motion"],
         date:"26 May, 2021",
-        imgSrc:"",
+        imgSrc:CavinImg,
         link:""
     },
     {
@@ -36,7 +42,7 @@ export const Blogs = [
         name:"What is Higher Order Component (HOC) in React JS?",
         tags:["react","reactJS","web-development"],
         date:"3 Feb, 2021",
-        imgSrc:"",
+        imgSrc:ProjectImg,
         link:""
     },
   
